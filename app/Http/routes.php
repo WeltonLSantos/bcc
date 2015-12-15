02fbab5d3d76bef321334bfe07ac5db66244b76f
Welton Santos <welton.santos.bcc@gmail.com>
2015-12-15 12:50:43 -0200

    site de estudo

---
+++ app/Http/routes.php
@@ -0,0 +1,28 @@
+<?php
+
+/*
+|--------------------------------------------------------------------------
+| Application Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register all of the routes for an application.
+| It's a breeze. Simply tell Laravel the URIs it should respond to
+| and give it the controller to call when that URI is requested.
+|
+*/
+
+Route::get('postagem',function()
+{
+    return'Listando as postagem';
+});
+
+Route::get('/', function () {
+    return view('exemplo');
+});
+
+Route::get('postagem/adicionar', function()
+{
+    return 'Adicionar uma nova postagem';
+});
+
+Route::get('posts','PosterControler@index');
